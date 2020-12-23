95be18cb970545036e15ec236c715814db12577b
x-bessie <qwas002@126.com>
2020-12-24 01:09:30 +0800

    删除文件

---
+++ src/main/java/com/bessie/comperhensive/ComperhensiveApplication.java
@@ -0,0 +1,13 @@
+package com.bessie.comperhensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComperhensiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComperhensiveApplication.class, args);
+    }
+
+}
